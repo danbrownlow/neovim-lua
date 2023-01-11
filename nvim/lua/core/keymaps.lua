2c6beee3e68366be0de8554b9cb63be77f15cce6
Daniel Brownlow <hello@danbrownlow.uk>
2023-01-11 18:07:21 +0000

    Add Telescope and bindings

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -67,3 +67,10 @@ map('n', '<leader>n', ':NvimTreeFindFile<CR>')      -- search file
 
 -- Tagbar
 map('n', '<leader>z', ':TagbarToggle<CR>')          -- open/close
+
+-- Telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
